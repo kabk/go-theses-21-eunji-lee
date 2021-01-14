b5f4e36664303cc7d777fc0bb0256d5e09c6ebc2
Eunji Lee <dldmswl5451@gmail.com>
2021-01-14 21:27:51 +0100

    codepen

---
+++ 1/scripts/mainscript.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("#dotspecial").click(function(){
+    $(".thesis").css("visibility","visible");
+  });
+});
+
+
+//
+//
+// $("#dot").on("click",function(){
+//   $(".thesis").show();
+// });
+//
+// function showThesis() {
+//   $(".thesis").show();
+// }
+
+//
+//
+// document.getElementById("footnote").onclick = function() {myFunction()};
+//
+// function myFunction() {
+//   document.getElementById("footnote").innerHTML = "YOU CLICKED ME!";
+// }
---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $(".abstract").click(function(){
+    $(".abstract").css("visibility","visible");
+    $("span.abstract").css("visibility","visible");
+  });
+
+  $(".prologue").click(function(){
+    $(".prologue").css("visibility","visible");
+    // $("span.prologue").css("visibility","visible");
+  });
+
+
+});
+
+
+//
+//
+// $("#dot").on("click",function(){
+//   $(".thesis").show();
+// });
+//
+// function showThesis() {
+//   $(".thesis").show();
+// }
